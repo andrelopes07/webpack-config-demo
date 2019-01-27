1c12fe22de602e75efa441c3c4a16a7462bea0ea
André Lopes <dx_andrelopes@hotmail.com>
2019-01-27 17:18:08 +0000

    Initial Commit

---
+++ src/containers/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Main Page</h1>
+                <p>App created for the sole purpose of toying around with webpack / babel configs </p>
+            </div>
+        );
+    }
+}
+
+export default Main;
